fa95a4d451ec2a088011148557dfdceffec4a7b3
lxt <terrence.luo@coassets.com.cn>
2019-08-01 17:35:38 +0800

    init

--- test_travis/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-from django.test.runner import DiscoverRunner
-
-
-class DJCETestSuiteRunner(DiscoverRunner):
-    def setup_test_environment(self, **kwargs):
-        # have to do this here as the default test runner overrides EMAIL_BACKEND
-        super(DJCETestSuiteRunner, self).setup_test_environment(**kwargs)
--- test_travis/tests/tests.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-from django.test import TestCase
-
-
-class SomethingTests(TestCase):
-    def test_something(self):
-        print('success')
